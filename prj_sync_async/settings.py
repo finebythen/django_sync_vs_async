dae92e6b15d6581157a11f7622e93d7380037988
Finn Then <71402477+finebythen@users.noreply.github.com>
2022-02-19 15:58:50 +0100

    Update settings.py

--- prj_sync_async/settings.py
+++ prj_sync_async/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s+$=lvc4(kz_fu=be2b8%7u36=^-kyej^rjc%$$2)ou@2&6_4l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
